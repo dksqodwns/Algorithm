30537f32d58af3a6389d92c97b5469ca64ea4f9f
ByeongJun Ahn <129524939+dksqodwns@users.noreply.github.com>
2024-02-18 23:07:57 +0900

    Time: 740 ms (44.59%), Space: 64.7 MB (77.25%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,20 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # 결과 조합을 저장할 리스트
         result = []
-        num_list = list(range(1, n+1))
+
+        # 1부터 n까지의 숫자를 포함하는 리스트
+        n_list = list(range(1, n+1))
         
-        def dfs(current, start):
-            if len(current) == k:
-                result.append(current)
+        def dfs(list, start):
+            # 현재 조합의 길이가 k와 같으면 result에 추가
+            if len(list) == k:
+                result.append(list)
                 return
             
-            for i in range(start, len(num_list)):
-                dfs(current + [num_list[i]], i + 1)
+            # start 인덱스부터 num_list의 끝까지 순회
+            for i in range(start, len(n_list)):
+                dfs(list + [n_list[i]], i + 1)
         
         dfs([], 0)
-        return result
 
-        
-        
-    # 1 ~ n 범위의 정수로 이루어진 길이가 k인 배열의 조합
-    # 단, 중복의 조합은 제외한다.
+        return result
