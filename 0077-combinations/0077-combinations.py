9cd6c0f5ed72871b97c1a74c5ebb5b8379c9b84f
ByeongJun Ahn <129524939+dksqodwns@users.noreply.github.com>
2024-02-18 22:59:17 +0900

    Time: 812 ms (25.02%), Space: 64.8 MB (77.25%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
-        
         num_list = list(range(1, n+1))
         
-        def dfs(list, curr):
-            if len(list) == k:
-                result.append(list)
+        def dfs(current, start):
+            if len(current) == k:
+                result.append(current)
                 return
-            for num in num_list[curr:]:
-                dfs(list + [num], num)
+            
+            for i in range(start, len(num_list)):
+                dfs(current + [num_list[i]], i + 1)
         
-        dfs(list=[], curr=0)
-        print(result)
+        dfs([], 0)
         return result
+
         
         
     # 1 ~ n 범위의 정수로 이루어진 길이가 k인 배열의 조합
